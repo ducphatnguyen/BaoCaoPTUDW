48681dcc48035968278f7bb0efa55c7245ea8398
ducphatnguyen <duc894580@gmail.com>
2023-05-04 11:15:28 +0700

    modified

--- Server/app/routes/admin.route.js
+++ Server/app/routes/admin.route.js
@@ -4,7 +4,7 @@ const admin = require("../controllers/admin.controller");
 const router = express.Router();
 
 router.route("/signin").post(admin.signIn);
-router.route("/signup").post(admin.signUp);
+// router.route("/signup").post(admin.signUp);
 router.route("/signout").post(admin.signOut);
 
 module.exports = router;
